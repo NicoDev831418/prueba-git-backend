c00fadd3a9eedeeb408f350d1a1d1bb71091b0a8
NicoDev <nicocarreta050@gmail.com>
2023-12-19 14:00:46 -0300

    Agrego clase persona solo name

---
+++ src/Persona.java
@@ -0,0 +1,18 @@
+public class Persona {
+    private String name;
+
+    public Persona() {
+    }
+
+    public Persona(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
